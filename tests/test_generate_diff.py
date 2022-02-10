41602da721d3c81c5e316544e7fe90b79de1f356
Andrey_Fominykh <meriadok2003@mail.ru>
2022-02-10 12:06:47 +0300

    lint tests directory

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -3,14 +3,11 @@ import json
 from gendiff import generate_diff
 
 
-
 test_data = [
     ('tests/fixtures/file1.json', 'tests/fixtures/file2.json',
-    'stylish',
-    'tests/fixtures/plane_diff.txt'),
+    'stylish', 'tests/fixtures/plane_diff.txt'),
     ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml',
-    'stylish',
-    'tests/fixtures/plane_diff.txt'),
+    'stylish', 'tests/fixtures/plane_diff.txt'),
     ('tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json',
     'stylish', 'tests/fixtures/nested_diff.txt'),
     ('tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml',
@@ -19,31 +16,31 @@ test_data = [
     'plain', 'tests/fixtures/nested_diff_in_plain.txt'),
     ('tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml',
     'plain', 'tests/fixtures/nested_diff_in_plain.txt'),
-    ]
+]
 
 test_data_json = [
     ('tests/fixtures/file1_nested.json', 'tests/fixtures/file2_nested.json',
     'json', 'tests/fixtures/nested_diff_in_json.txt'),
     ('tests/fixtures/file1_nested.yaml', 'tests/fixtures/file2_nested.yaml',
     'json', 'tests/fixtures/nested_diff_in_json.txt'),
-    ]
+]
 
 
-def get_correct_file(path, format_name):
+def get_correct_file(path, fmt):
     with open(path) as file:
-        if format_name == 'json':
+        if fmt == 'json':
             return json.loads(file.read())
         else:
             return file.read()
 
 
-@pytest.mark.parametrize('file1, file2, format_name, correct_file_path', test_data_json)
-def test_generate_diff_in_json(file1, file2, format_name, correct_file_path):
-    correct_file = get_correct_file(correct_file_path, format_name)
-    assert json.loads(generate_diff(file1, file2, format_name)) == correct_file
+@pytest.mark.parametrize('file1, file2, fmt, correct_path', test_data_json)
+def test_generate_diff_in_json(file1, file2, fmt, correct_path):
+    correct_file = get_correct_file(correct_path, fmt)
+    assert json.loads(generate_diff(file1, file2, fmt)) == correct_file
 
 
-@pytest.mark.parametrize('file1, file2, format_name, correct_file_path', test_data)
-def test_generate_diff_other_formats(file1, file2, format_name, correct_file_path):
-    correct_file = get_correct_file(correct_file_path, format_name)
-    assert generate_diff(file1, file2, format_name) == correct_file
+@pytest.mark.parametrize('file1, file2, fmt, correct_path', test_data)
+def test_generate_diff_other(file1, file2, fmt, correct_path):
+    correct_file = get_correct_file(correct_path, fmt)
+    assert generate_diff(file1, file2, fmt) == correct_file
